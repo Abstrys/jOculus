b4fae4be99f6c0965b6243632395ef58c7018458
Eron Hennessey <eron@abstrys.com>
2012-08-13 08:04:18 -0700

    Temp (non-working) commit. Errors in AntiWord.java

--- src/abstrys/antiword/Settings.java
+++ src/abstrys/antiword/Settings.java
@@ -4,7 +4,21 @@ import java.awt.Dimension;
 
 class Settings
 {
-   public static Dimension default_size = new Dimension(400,600);
+   public static Dimension default_size = new Dimension(400, 500);
    public static Color action_panel_color = Color.darkGray;
-   public static String md_processor_cmd = "pandoc";
+   public static String md_processor_cmd = "/usr/local/bin/pandoc --from markdown --to html";
+   public static boolean display_word_count = true;
+   
+   public Settings()
+   {
+       setDefaults();
+   }
+   
+   public void setDefaults()
+   {
+    default_size = new Dimension(400,600);
+    action_panel_color = Color.darkGray;
+    md_processor_cmd = "/usr/local/bin/pandoc --from markdown --to html";
+    display_word_count = true;
+   }
 }
